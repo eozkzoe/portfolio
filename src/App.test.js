6dff2e000af4bc1da226ad0b5cc3adc53675b63f
Ethan Ong <36510144+eozkzoe@users.noreply.github.com>
2026-05-01 19:15:56 +0800

    Revert "Add Shopee Project Manager experience"

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders name', () => {
-  render(<App />);
-  const linkElement = screen.getAllByText(/Ethan/i)[0];
-  expect(linkElement).toBeInTheDocument();
-});
